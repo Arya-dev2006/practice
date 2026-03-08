85e7ef5cda996eca45f297f4a57e4f7e799b31e6
Arya Chakraborty <arya021chakraborty@gmail.com>
2026-03-08 15:52:08 +0530

    completed hero section

--- Frontend/src/components/Section1.jsx
+++ Frontend/src/components/Section1.jsx
@@ -6,10 +6,28 @@ const Section1 = () => {
         <div id="center">
             <h1>Unleash your inner Champion today.</h1>
             <h1>All in one place</h1>
-            <p>join the ultimate tennis experience- where passion meets performance.
-            </p>
+            <div className="subtitle">
+            <p>join the ultimate tennis experience- where passion meets performance.</p>
             <p>and every swing brings you closer to victory.</p>
+            </div>
             <button>Start your own journey</button>
+        </div>
+        <div id="bottom">
+            <div id="text">
+                 <p>train with real professional. <br />get the real results.</p>
+                <div id="icons">
+                <div className="images"></div>
+                <div className="images"></div>
+                <div className="images"></div>
+            </div>
+            </div>
+            <div id="socials">
+                <div className="social-links"><p>Facebook</p></div>
+                <div className="social-links"><p>Instagram</p></div>
+                <div className="social-links"><p>twitter</p></div>
+            </div>
+           
+           
         </div>
     </div>
   )
